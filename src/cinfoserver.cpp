dfdb3a17fffb6aeb6230b8e7afff9010120580be
RoLex <webmaster@feardc.net>
2014-07-16 00:20:15 +0200

    Add hub locale to hub information

--- src/cinfoserver.cpp
+++ src/cinfoserver.cpp
@@ -1,6 +1,7 @@
 /*
 	Copyright (C) 2003-2005 Daniel Muller, dan at verliba dot cz
-	Copyright (C) 2006-2014 Verlihub Project, devs at verlihub-project dot org
+	Copyright (C) 2006-2012 Verlihub Team, devs at verlihub-project dot org
+	Copyright (C) 2013-2014 RoLex, webmaster at feardc dot net
 
 	Verlihub is free software; You can redistribute it
 	and modify it under the terms of the GNU General
@@ -61,9 +62,9 @@ void cInfoServer::SystemInfo(ostream &os)
 
 	os << "\r\n [*] " << setw(PADDING) << setiosflags(ios::left) << _("System uptime") << uptime.AsPeriod().AsString().c_str() << endl;
 	os << " [*] " << setw(PADDING) << setiosflags(ios::left) << _("Load averages") << autosprintf("%.2f %.2f %.2f", serverInfo.loads[0]/65536.0, serverInfo.loads[1]/65536.0, serverInfo.loads[2]/65536.0) << endl;
-	os << " [*] " << setw(PADDING) << setiosflags(ios::left) << _("Total RAM") << convertByte((long long int) serverInfo.totalram, false).c_str() << endl;
-	os << " [*] " << setw(PADDING) << setiosflags(ios::left) << _("Free RAM") << convertByte((long long int) serverInfo.freeram, false).c_str() << endl;
-	os << " [*] " << setw(PADDING) << setiosflags(ios::left) << _("Shared RAM") << convertByte((long long int) serverInfo.sharedram, false).c_str() << endl;
+	os << " [*] " << setw(PADDING) << setiosflags(ios::left) << _("Total memory") << convertByte((long long int) serverInfo.totalram, false).c_str() << endl;
+	os << " [*] " << setw(PADDING) << setiosflags(ios::left) << _("Free memory") << convertByte((long long int) serverInfo.freeram, false).c_str() << endl;
+	os << " [*] " << setw(PADDING) << setiosflags(ios::left) << _("Shared memory") << convertByte((long long int) serverInfo.sharedram, false).c_str() << endl;
 	os << " [*] " << setw(PADDING) << setiosflags(ios::left) << _("Memory in buffers") << convertByte((long long int) serverInfo.bufferram, false).c_str() << endl;
 	os << " [*] " << setw(PADDING) << setiosflags(ios::left) << _("Free swap") << convertByte((long long int) serverInfo.freeswap, false).c_str() << "/" << convertByte((long long int) serverInfo.totalswap, false).c_str() << endl;
 	os << " [*] " << setw(PADDING) << setiosflags(ios::left) << _("Number of processes") << serverInfo.procs;
@@ -85,10 +86,10 @@ void cInfoServer::Output(ostream &os, int Class)
 	iterator it;
 	cTime theTime;
 	os << _("Hub information") << ":";
-	os << "\r\n [*] " << setw(PADDING) << setiosflags(ios::left) << _("Version date") << __CURR_DATE_TIME__ << endl;
+	os << "\r\n [*] " << setw(PADDING) << setiosflags(ios::left) << _("Version") << VERSION << " @ " << __CURR_DATE_TIME__ << "\r\n";
 	theTime = mServer->mTime;
 	theTime -= mServer->mStartTime;
-	os << " [*] " << setw(PADDING) << setiosflags(ios::left) << _("Uptime") << theTime.AsPeriod().AsString().c_str() << endl;
+	os << " [*] " << setw(PADDING) << setiosflags(ios::left) << _("Uptime") << theTime.AsPeriod().AsString().c_str() << "\r\n";
 	string loadType;
 
 	if (mServer->mSysLoad >= eSL_RECOVERY)
@@ -102,8 +103,8 @@ void cInfoServer::Output(ostream &os, int Class)
 	else
 		loadType = _("Not available");
 
-	os << " [*] " << setw(PADDING) << setiosflags(ios::left) << _("Server frequency") << mServer->mFrequency.GetMean(mServer->mTime) << " " << "(" << loadType.c_str() << ")" << endl;
-	os << " [*] " << setw(PADDING) << setiosflags(ios::left) << _("Hub name") << mServer->mC.hub_name << endl;
+	os << " [*] " << setw(PADDING) << setiosflags(ios::left) << _("Server frequency") << mServer->mFrequency.GetMean(mServer->mTime) << " " << "(" << loadType.c_str() << ")" << "\r\n";
+	os << " [*] " << setw(PADDING) << setiosflags(ios::left) << _("Hub name") << mServer->mC.hub_name << "\r\n";
 	string hubOwner;
 
 	if (!mServer->mC.hub_owner.empty())
@@ -111,7 +112,7 @@ void cInfoServer::Output(ostream &os, int Class)
 	else
 		hubOwner = "--";
 
-	os << " [*] " << setw(PADDING) << setiosflags(ios::left) << _("Hub owner") << hubOwner << endl;
+	os << " [*] " << setw(PADDING) << setiosflags(ios::left) << _("Hub owner") << hubOwner << "\r\n";
 	string hubCategory;
 
 	if (!mServer->mC.hub_category.empty())
@@ -119,18 +120,26 @@ void cInfoServer::Output(ostream &os, int Class)
 	else
 		hubCategory = "--";
 
-	os << " [*] " << setw(PADDING) << setiosflags(ios::left) << _("Hub category") << hubCategory << endl;
-	os << "    " << string(30,'=') << endl;
-	os << " [*] " << setw(PADDING) << setiosflags(ios::left) << _("Current online users") << mServer->mUserCountTot << "/" << mServer->mC.max_users_total << endl;
-	os << " [*] " << setw(PADDING) << setiosflags(ios::left) << _("Current share total") << convertByte(mServer->mTotalShare, false).c_str() << endl;
-	os << " [*] " << setw(PADDING) << setiosflags(ios::left) << _("User list count") << mServer->mUserList.Size() << endl;
-	os << " [*] " << setw(PADDING) << setiosflags(ios::left) << _("Hello user count") << mServer->mHelloUsers.Size() << endl;
-	os << " [*] " << setw(PADDING) << setiosflags(ios::left) << _("In progress users") << mServer->mInProgresUsers.Size() << endl;
-	os << " [*] " << setw(PADDING) << setiosflags(ios::left) << _("Active user count") << mServer->mActiveUsers.Size() << endl;
-	os << " [*] " << setw(PADDING) << setiosflags(ios::left) << _("Passive user count") << mServer->mPassiveUsers.Size() << endl;
-	os << " [*] " << setw(PADDING) << setiosflags(ios::left) << _("OP user count") << mServer->mOpchatList.Size() << endl; // todo: why is mOpchatList used here instead of mOpList?
-	os << " [*] " << setw(PADDING) << setiosflags(ios::left) <<_("Bot user count") << mServer->mRobotList.Size() << endl;
-	os << "    " << string(30,'=') << endl;
+	os << " [*] " << setw(PADDING) << setiosflags(ios::left) << _("Hub category") << hubCategory << "\r\n";
+	string hubLocale;
+
+	if (!mServer->mDBConf.locale.empty())
+		hubLocale = mServer->mDBConf.locale;
+	else
+		hubLocale = "--";
+
+	os << " [*] " << setw(PADDING) << setiosflags(ios::left) << _("Hub locale") << hubLocale << "\r\n";
+	os << "    " << string(30,'=') << "\r\n";
+	os << " [*] " << setw(PADDING) << setiosflags(ios::left) << _("Online users") << mServer->mUserCountTot << " " << _("of") << " " << mServer->mC.max_users_total << "\r\n";
+	os << " [*] " << setw(PADDING) << setiosflags(ios::left) << _("Total share") << convertByte(mServer->mTotalShare, false).c_str() << "\r\n";
+	os << " [*] " << setw(PADDING) << setiosflags(ios::left) << _("User list count") << mServer->mUserList.Size() << "\r\n";
+	os << " [*] " << setw(PADDING) << setiosflags(ios::left) << _("Hello user count") << mServer->mHelloUsers.Size() << "\r\n";
+	os << " [*] " << setw(PADDING) << setiosflags(ios::left) << _("In progress users") << mServer->mInProgresUsers.Size() << "\r\n";
+	os << " [*] " << setw(PADDING) << setiosflags(ios::left) << _("Active user count") << mServer->mActiveUsers.Size() << "\r\n";
+	os << " [*] " << setw(PADDING) << setiosflags(ios::left) << _("Passive user count") << mServer->mPassiveUsers.Size() << "\r\n";
+	os << " [*] " << setw(PADDING) << setiosflags(ios::left) << _("Operator user count") << mServer->mOpchatList.Size() << "\r\n"; // todo: why is mOpchatList used here instead of mOpList?
+	os << " [*] " << setw(PADDING) << setiosflags(ios::left) <<_("Bot user count") << mServer->mRobotList.Size() << "\r\n";
+	os << "    " << string(30,'=') << "\r\n";
 	double total = 0, curr;
 
 	// print zone from 1 to 3
@@ -141,9 +150,9 @@ void cInfoServer::Output(ostream &os, int Class)
 			os << " [*] " << setw(PADDING) << setiosflags(ios::left) << autosprintf(_("Users in zone #%d"), i) << mServer->mUserCount[i] << "/" << mServer->mC.max_users[i];
 			curr = mServer->mUploadZone[i].GetMean(mServer->mTime);
 			total += curr;
-			os << " (" << convertByte(curr, true).c_str() << ")" << " [" << zone.c_str() << "]" << endl;
+			os << " (" << convertByte(curr, true).c_str() << ")" << " [" << zone.c_str() << "]" << "\r\n";
 		} else
-			os << " [*] " << setw(PADDING) << setiosflags(ios::left) << autosprintf(_("Users in zone #%d"), i) << _("Not set") << " " << _("[CC]") << endl;
+			os << " [*] " << setw(PADDING) << setiosflags(ios::left) << autosprintf(_("Users in zone #%d"), i) << _("Not set") << " " << _("[CC]") << "\r\n";
 	}
 
 	// print zone from 4 to 6
@@ -151,7 +160,7 @@ void cInfoServer::Output(ostream &os, int Class)
 		curr = mServer->mUploadZone[i].GetMean(mServer->mTime);
 		total += curr;
 		os << " [*] " << setw(PADDING) << setiosflags(ios::left) << autosprintf(_("Users in zone #%d"), i) << mServer->mUserCount[i] << "/" << mServer->mC.max_users[i];
-		os << " (" << convertByte(curr, true).c_str() << ") " << _("[IP]") << endl;
+		os << " (" << convertByte(curr, true).c_str() << ") " << _("[IP]") << "\r\n";
 	}
 
 	// print zone 0
