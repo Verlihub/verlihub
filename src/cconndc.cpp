f035d20e3e5d878353016dac1ab68a05ea14ad99
RoLex <webmaster@feardc.net>
2017-02-06 09:32:19 +0100

    Undo the test

--- src/cconndc.cpp
+++ src/cconndc.cpp
@@ -60,11 +60,6 @@ cConnDC::cConnDC(int sd, cAsyncSocketServer *server):
 
 cConnDC::~cConnDC()
 {
-	mBufFlush.erase(0, GetFlushSize());
-	ShrinkStringToFit(mBufFlush);
-	mBufSend.erase(0, GetBufferSize());
-	ShrinkStringToFit(mBufSend);
-
 	if (mRegInfo) {
 		delete mRegInfo;
 		mRegInfo = NULL;
