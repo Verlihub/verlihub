1d98b8082e4a403bb3c4ddc70916e2f0634dff51
RoLex <webmaster@feardc.net>
2018-01-14 22:28:25 +0100

    Add MaxMindDB optimizations, avoid looking up same IP address and conversion of ASCII characters Add hub listen ports check to avoid duplicates

--- src/casyncconn.h
+++ src/casyncconn.h
@@ -1,6 +1,6 @@
 /*
 	Copyright (C) 2003-2005 Daniel Muller, dan at verliba dot cz
-	Copyright (C) 2006-2017 Verlihub Team, info at verlihub dot net
+	Copyright (C) 2006-2018 Verlihub Team, info at verlihub dot net
 
 	Verlihub is free software; You can redistribute it
 	and modify it under the terms of the GNU General
@@ -356,7 +356,7 @@ namespace nVerliHub {
 				 * @param addr IP address in network byte order.
 				 * @return The IP address.
 				 */
-				static string IPAsString(unsigned long addr);
+				//static string IPAsString(unsigned long addr);
 
 				/**
 				* Return the status of the line.
