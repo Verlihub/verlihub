be35e63c6bfc2c8b01379e37c3bc9dc20d730771
RoLex <webmaster@feardc.net>
2018-02-24 12:52:09 +0100

    Fix proto_flood_report_time config

--- src/cmysql.cpp
+++ src/cmysql.cpp
@@ -1,6 +1,6 @@
 /*
 	Copyright (C) 2003-2005 Daniel Muller, dan at verliba dot cz
-	Copyright (C) 2006-2017 Verlihub Team, info at verlihub dot net
+	Copyright (C) 2006-2018 Verlihub Team, info at verlihub dot net
 
 	Verlihub is free software; You can redistribute it
 	and modify it under the terms of the GNU General
@@ -20,7 +20,6 @@
 
 #include "cmysql.h"
 #include "cconfmysql.h"
-#include "mysql_version.h"
 #include "i18n.h"
 
 namespace nVerliHub {
