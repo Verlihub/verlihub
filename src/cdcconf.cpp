1ed4e5640e7df74c2946512b0c66df3ce3cf6c45
Pavel Pimenov <pavel-pimenov@users.noreply.github.com>
2018-12-29 18:11:04 +0300

    Remove string("") (#162)

--- src/cdcconf.cpp
+++ src/cdcconf.cpp
@@ -40,22 +40,22 @@ void cDCConf::AddVars()
 {
 	// Hub info and basic settings
 	Add("hub_name", hub_name, HUB_VERSION_NAME);
-	Add("hub_desc",hub_desc,string(""));
-	Add("hub_topic",hub_topic,string(""));
-	Add("hub_category",hub_category,string(""));
-	Add("hub_icon_url", hub_icon_url, string(""));
-	Add("hub_logo_url", hub_logo_url, string(""));
+	Add("hub_desc",hub_desc,string());
+	Add("hub_topic",hub_topic,string());
+	Add("hub_category",hub_category,string());
+	Add("hub_icon_url", hub_icon_url, string());
+	Add("hub_logo_url", hub_logo_url, string());
 	Add("hub_encoding", hub_encoding, DEFAULT_HUB_ENCODING);
-	Add("hub_owner",hub_owner,string(""));
+	Add("hub_owner",hub_owner,string());
 	Add("hub_version", hub_version, HUB_VERSION_VERS);
-	Add("hub_version_special",hub_version_special,string(""));
+	Add("hub_version_special",hub_version_special,string());
 	Add("hub_security", hub_security, HUB_VERSION_NAME);
 	Add("hub_security_desc", hub_security_desc, string("Hub security"));
 	Add("opchat_name", opchat_name, string("OpChat"));
 	Add("opchat_desc", opchat_desc, string("Operator chat"));
 	Add("opchat_class", opchat_class, int(eUC_OPERATOR));
-	Add("hub_host", hub_host, string(""));
-	Add("hub_failover_hosts", hub_failover_hosts, string(""));
+	Add("hub_host", hub_host, string());
+	Add("hub_failover_hosts", hub_failover_hosts, string());
 	Add("listen_ip",mS.mAddr,string("0.0.0.0"));
 
 	//#if !defined _WIN32
@@ -66,7 +66,7 @@ void cDCConf::AddVars()
 	#endif
 	*/
 
-	Add("extra_listen_ports", extra_listen_ports,string(""));
+	Add("extra_listen_ports", extra_listen_ports,string());
 	// End hub info and basic settings
 
 	// begin hublist configuration
@@ -130,11 +130,11 @@ void cDCConf::AddVars()
 	// Nicklist configuration
 	Add("max_nick",max_nick,64u);
 	Add("min_nick",min_nick,1u);
-	Add("nick_chars",nick_chars, string(""));
-	Add("nick_prefix", nick_prefix, string(""));
+	Add("nick_chars",nick_chars, string());
+	Add("nick_prefix", nick_prefix, string());
 	Add("nick_prefix_nocase", nick_prefix_nocase, false);
 	Add("nick_prefix_cc", nick_prefix_cc, false);
-	Add("nick_prefix_autoreg",nick_prefix_autoreg,string(""));
+	Add("nick_prefix_autoreg",nick_prefix_autoreg,string());
 	Add("autoreg_class", autoreg_class, 0);
 	Add("autounreg_class", autounreg_class, 0);
 	// End nicklist configuration
@@ -352,7 +352,7 @@ void cDCConf::AddVars()
 	Add("zlib_min_len", zlib_min_len, 100);
 	Add("detect_ctmtohub", detect_ctmtohub, true); // ctm2hub
 	Add("disable_extjson", disable_extjson, true); // extjson
-	Add("mmdb_names_lang", mmdb_names_lang, string("")); // maxminddb names language, empty means english
+	Add("mmdb_names_lang", mmdb_names_lang, string()); // maxminddb names language, empty means english
 	Add("mmdb_conv_depth", mmdb_conv_depth, 2); // conversion depth, 0 = do nothing, 1 = utf8 to hub_encoding conversion, 2 = transliteration
 	Add("mmdb_cache", mmdb_cache, true);
 	Add("mmdb_cache_mins", mmdb_cache_mins, 60); // 0 = never delete
@@ -384,7 +384,7 @@ void cDCConf::AddVars()
 	Add("tag_min_class_ignore", tag_min_class_ignore, (int)eUC_OPERATOR);
 	Add("show_desc_len",show_desc_len,-1);
 	Add("desc_insert_mode", desc_insert_mode, false);
-	Add("desc_insert_vars", desc_insert_vars, string("")); // %[CLASS], %[CLASSNAME], %[MODE], %[CC], %[CN], %[CITY]
+	Add("desc_insert_vars", desc_insert_vars, string()); // %[CLASS], %[CLASSNAME], %[MODE], %[CC], %[CN], %[CITY]
 	Add("show_email", show_email, true);
 	Add("show_speed", show_speed, true);
 	Add("tag_min_hs_ratio", tag_min_hs_ratio, 0.);
@@ -399,20 +399,20 @@ void cDCConf::AddVars()
 	// End tag configuration
 
 	// IP and zone configuration
-	Add("cc_zone1",cc_zone[0],string(""));
-	Add("cc_zone2",cc_zone[1],string(""));
-	Add("cc_zone3",cc_zone[2],string(""));
-	Add("ip_zone4_min",ip_zone4_min,string(""));
-	Add("ip_zone4_max",ip_zone4_max,string(""));
-	Add("ip_zone5_min",ip_zone5_min,string(""));
-	Add("ip_zone5_max",ip_zone5_max,string(""));
-	Add("ip_zone6_min",ip_zone6_min,string(""));
-	Add("ip_zone6_max",ip_zone6_max,string(""));
+	Add("cc_zone1",cc_zone[0],string());
+	Add("cc_zone2",cc_zone[1],string());
+	Add("cc_zone3",cc_zone[2],string());
+	Add("ip_zone4_min",ip_zone4_min,string());
+	Add("ip_zone4_max",ip_zone4_max,string());
+	Add("ip_zone5_min",ip_zone5_min,string());
+	Add("ip_zone5_max",ip_zone5_max,string());
+	Add("ip_zone6_min",ip_zone6_min,string());
+	Add("ip_zone6_max",ip_zone6_max,string());
 	// End IP and zone configuration
 
 	// Custom messages
 	Add("ban_extra_message", ban_extra_message, "");
-	Add("msg_replace_ban",msg_replace_ban, string(""));
+	Add("msg_replace_ban",msg_replace_ban, string());
 	Add("msg_welcome_guest",msg_welcome[0]);
 	Add("msg_welcome_reg",msg_welcome[1]);
 	Add("msg_welcome_vip",msg_welcome[2]);
