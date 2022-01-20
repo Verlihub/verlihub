Verlihub/verlihub

commit bdb68eb13b09b3fb792717df2ba990f5cb24cae5

Get rid of USE_BUFFER_RESERVE
Add VH:SetMyINFOFlag(nick, flag) and VH:UnsetMyINFOFlag(nick, flag) in Lua and Python, to manipulate status flags in user $MyINFO (only works when executing on VH_OnParsedMsgMyINFO and VH_OnFirstMyINFO calls)
Properly filter secure TLS client to client connection requests (TLS version 1.0 is no longer compatible with 1.3)
Try to fix UTF-8 conversion in bad CTM requests from client supporting NAT traversal

CMakeLists.txt
@@ -23,7 +23,6 @@ message(STATUS)
 message(STATUS "You can change build options with: -D{OPTION}=ON|OFF")
 message(STATUS "You can turn on debug build with: -DDEFINE_DEBUG=ON")
 message(STATUS "You can disable all plugins with: -DWITH_PLUGINS=OFF")
-message(STATUS "You can turn on buffer reservation with: -DUSE_BUFFER_RESERVE=ON")
 #message(STATUS "You can disable SSL secured connects with: -DUSE_SSL_CONNECTS=OFF")
 message(STATUS "You can also disable specific plugins with: -DWITH_{NAME}=OFF")
 message(STATUS "Plugin names: LUA PYTHON PERL FORBID CHATROOM IPLOG ISP MESSENGER STATS REPLACER FLOODPROT")
@@ -33,7 +32,7 @@ message(STATUS)
 SET(VERLIHUB_VERSION_MAJOR 1) # note: these must be numbers, else compilation will fail
 SET(VERLIHUB_VERSION_MINOR 3)
 SET(VERLIHUB_VERSION_PATCH 1)
-SET(VERLIHUB_VERSION_TWEAK 0)
+SET(VERLIHUB_VERSION_TWEAK 1)
 SET(VERLIHUB_VERSION "${VERLIHUB_VERSION_MAJOR}.${VERLIHUB_VERSION_MINOR}.${VERLIHUB_VERSION_PATCH}.${VERLIHUB_VERSION_TWEAK}")
 #SET(VERLIHUB_SOVERSION "${VERLIHUB_VERSION_MAJOR}.${VERLIHUB_VERSION_MINOR}.${VERLIHUB_VERSION_PATCH}")
 SET(PROJECT_NAME "verlihub")
@@ -202,9 +201,6 @@ ENDIF(HAVE_SYSLOG_H)
 IF(NOT HAVE_GETOPT_H)
 	MESSAGE(FATAL_ERROR "[ ER ] getopt.h header not found, it is required to build Verlihub.")
 ENDIF(NOT HAVE_GETOPT_H)
-
-ADD_DEFINITIONS(-DUSE_BUFFER_RESERVE)
-OPTION(USE_BUFFER_RESERVE "Use buffer string reservation" OFF) # use cmake -DUSE_BUFFER_RESERVE=ON to use buffer string reservation
 
 OPTION(DEFINE_DEBUG "Build the project using debugging code" OFF) # use cmake -DDEFINE_DEBUG=ON to enable debug
 

plugins/chatroom/crooms.cpp
@@ -77,16 +77,11 @@ void cRoom::OnLoad()
 		mChatRoom->mClass = tUserCl(3);
 
 		if (mTopic.size()) {
-#ifdef USE_BUFFER_RESERVE
-			if (desc.capacity() < (desc.size() + 2 + mTopic.size()))
-				desc.reserve(desc.size() + 2 + mTopic.size());
-#endif
-
 			desc.append(": ");
 			desc.append(mTopic);
 		}
 
-		mServer->mP.Create_MyINFO(mChatRoom->mMyINFO, mNick, desc, speed, mail, share, false); // dont reserve for pipe, we are not sending this
+		mServer->mP.Create_MyINFO(mChatRoom->mFakeMyINFO, mNick, desc, speed, mail, share);
 		mPlugin->AddRobot(mChatRoom);
 	}
 }

plugins/floodprot/cpifloodprot.cpp
@@ -62,11 +62,7 @@ bool cpiFloodprot::RegisterAll()
 bool cpiFloodprot::OnNewConn(cConnDC *conn)
 {
 	if (!mFloodprotect->AddConn(conn, 1)) {
-		string omsg;
-#ifdef USE_BUFFER_RESERVE
-		omsg.reserve(63 + 1);
-#endif
-		omsg = "Sorry, the limit of connections from your IP has been exceeded."; // length = 63
+		string omsg("Sorry, the limit of connections from your IP has been exceeded.");
 		conn->Send(omsg, true);
 		conn->CloseNice(500); // not sure if this is needed
 		return false;
@@ -78,11 +74,7 @@ bool cpiFloodprot::OnNewConn(cConnDC *conn)
 bool cpiFloodprot::OnUserLogin(cUser *user)
 {
 	if (!mFloodprotect->AddConn(user->mxConn, 1)) {
-		string omsg;
-#ifdef USE_BUFFER_RESERVE
-		omsg.reserve(76 + 1);
-#endif
-		omsg = "Sorry, the limit of unregistered connections form your IP has been exceeded."; // length = 76
+		string omsg("Sorry, the limit of unregistered connections form your IP has been exceeded.");
 		user->mxConn->Send(omsg, true);
 		user->mxConn->CloseNice(500); // not sure if this is needed
 		return false;

plugins/lua/callbacks.cpp
@@ -1312,7 +1312,7 @@ int _GetOPList(lua_State *L)
 
 		if (server) {
 			string oplist;
-			server->mOpList.GetNickList(oplist, false);
+			server->mOpList.GetNickList(oplist);
 
 			if (oplist.empty())
 				result = 0;
@@ -1341,7 +1341,7 @@ int _GetBotList(lua_State *L)
 
 		if (server) {
 			string botlist;
-			server->mRobotList.GetNickList(botlist, false);
+			server->mRobotList.GetNickList(botlist);
 
 			if (botlist.empty())
 				result = 0;
@@ -1463,6 +1463,74 @@ int _SetUserIP(lua_State *L)
 	return 2;
 }
 
+int _SetMyINFOFlag(lua_State *L)
+{
+	int args = lua_gettop(L) - 1;
+
+	if (args < 2) {
+		luaL_error(L, "Error calling VH:SetMyINFOFlag, expected 2 arguments but got %d.", args);
+		lua_pushboolean(L, 0);
+		lua_pushnil(L);
+		return 2;
+	}
+
+	if (!lua_isstring(L, 2) || !lua_isnumber(L, 3)) {
+		luaerror(L, ERR_PARAM);
+		return 2;
+	}
+
+	const string nick = lua_tostring(L, 2);
+	const unsigned int flag = (unsigned int)lua_tonumber(L, 3);
+
+	if (nick.empty() || (flag < 1)) {
+		luaerror(L, ERR_EMPT);
+		return 2;
+	}
+
+	if (!SetMyINFOFlag(nick.c_str(), flag)) {
+		luaerror(L, ERR_CALL);
+		return 2;
+	}
+
+	lua_pushboolean(L, 1);
+	lua_pushnil(L);
+	return 2;
+}
+
+int _UnsetMyINFOFlag(lua_State *L)
+{
+	int args = lua_gettop(L) - 1;
+
+	if (args < 2) {
+		luaL_error(L, "Error calling VH:UnsetMyINFOFlag, expected 2 arguments but got %d.", args);
+		lua_pushboolean(L, 0);
+		lua_pushnil(L);
+		return 2;
+	}
+
+	if (!lua_isstring(L, 2) || !lua_isnumber(L, 3)) {
+		luaerror(L, ERR_PARAM);
+		return 2;
+	}
+
+	const string nick = lua_tostring(L, 2);
+	const unsigned int flag = (unsigned int)lua_tonumber(L, 3);
+
+	if (nick.empty() || (flag < 1)) {
+		luaerror(L, ERR_EMPT);
+		return 2;
+	}
+
+	if (!UnsetMyINFOFlag(nick.c_str(), flag)) {
+		luaerror(L, ERR_CALL);
+		return 2;
+	}
+
+	lua_pushboolean(L, 1);
+	lua_pushnil(L);
+	return 2;
+}
+
 int _IsSecConn(lua_State *L)
 {
 	int args = lua_gettop(L) - 1;
@@ -1551,8 +1619,9 @@ int _IsUserOnline(lua_State *L)
 		}
 		string nick = lua_tostring(L, 2);
 		cUser *usr = server->mUserList.GetUserByNick(nick);
+		lua_pushboolean(L, 1);
 		lua_pushboolean(L, (usr == NULL ? 0 : 1));
-		return 1;
+		return 2;
 	} else {
 		luaL_error(L, "Error calling VH:IsUserOnline; expected 1 argument but got %d", lua_gettop(L) - 1);
 		lua_pushboolean(L, 0);
@@ -2226,7 +2295,7 @@ int _RegBot(lua_State *L)
 		shar = IntToStr(share);
 	}
 
-	serv->mP.Create_MyINFO(temp, nick, desc, conn, mail, shar, false); // create myinfo, dont reserve for pipe, we are not sending this
+	serv->mP.Create_MyINFO(temp, nick, desc, conn, mail, shar); // create myinfo
 
 	if (!plug->NewRobot(nick, clas, temp)) { // note: this will show user to all
 		luaerror(L, "Error registering bot");
@@ -2343,7 +2412,7 @@ int _EditBot(lua_State *L)
 			if (!serv->mOpList.ContainsHash(robot->mNickHash)) { // add to oplist
 				serv->mOpList.AddWithHash(robot, robot->mNickHash);
 
-				serv->mP.Create_OpList(temp, nick, true); // send short oplist, reserve for pipe
+				serv->mP.Create_OpList(temp, nick); // send short oplist
 				serv->mUserList.SendToAll(temp, serv->mC.delayed_myinfo, true);
 			}
 
@@ -2351,14 +2420,14 @@ int _EditBot(lua_State *L)
 			if (serv->mOpList.ContainsHash(robot->mNickHash)) { // remove from oplist
 				serv->mOpList.RemoveByHash(robot->mNickHash);
 
-				serv->mP.Create_Quit(temp, nick, true); // send quit, reserve for pipe
+				serv->mP.Create_Quit(temp, nick); // send quit
 				serv->mUserList.SendToAll(temp, serv->mC.delayed_myinfo, true);
 
-				serv->mP.Create_BotList(temp, nick, true); // send short botlist after quit, reserve for pipe
+				serv->mP.Create_BotList(temp, nick); // send short botlist after quit
 				serv->mUserList.SendToAllWithFeature(temp, eSF_BOTLIST, serv->mC.delayed_myinfo, true);
 
 				if (serv->mC.send_user_ip) { // send userip to operators, bots have local ip
-					serv->mP.Create_UserIP(temp, nick, "127.0.0.1", true); // reserve for pipe
+					serv->mP.Create_UserIP(temp, nick, "127.0.0.1");
 					serv->mUserList.SendToAllWithClassFeature(temp, serv->mC.user_ip_class, eUC_MASTER, eSF_USERIP2, serv->mC.delayed_myinfo, true); // must be delayed too
 				}
 			}
@@ -2367,17 +2436,10 @@ int _EditBot(lua_State *L)
 		robot->mClass = (tUserCl)clas; // set new class
 	}
 
-	serv->mP.Create_MyINFO(robot->mMyINFO, nick, desc, conn, mail, shar, false); // send new myinfo after quit, dont reserve for pipe, we are not sending this
-
-#ifdef USE_BUFFER_RESERVE
-	if (temp.capacity() < (robot->mMyINFO.size() + 1)) // reserve for pipe
-		temp.reserve(robot->mMyINFO.size() + 1);
-#endif
-
-	temp = robot->mMyINFO;
+	serv->mP.Create_MyINFO(robot->mFakeMyINFO, nick, desc, conn, mail, shar);
+	temp = robot->mFakeMyINFO;
 	serv->mUserList.SendToAll(temp, serv->mC.delayed_myinfo, true); // show new myinfo to all
-
-	lua->editBot(nick.c_str(), robot->mMyINFO.c_str(), share, clas); // edit in lua bots
+	lua->editBot(nick.c_str(), robot->mFakeMyINFO.c_str(), share, clas); // edit in lua bots
 	lua_pushboolean(L, 1);
 	lua_pushnil(L);
 	return 2;
@@ -3123,7 +3185,7 @@ int _SetTopic(lua_State *L)
 
 	string topic = lua_tostring(L, 2);
 	string omsg;
-	serv->mP.Create_HubName(omsg, serv->mC.hub_name, topic, false); // dont reserve for pipe, buffer is copied before adding pipe
+	serv->mP.Create_HubName(omsg, serv->mC.hub_name, topic);
 	serv->SendToAll(omsg, eUC_NORMUSER, eUC_MASTER);
 	SetConfig(li->mConfigName.c_str(), "hub_topic", topic.c_str());
 	lua_pushboolean(L, 1);

plugins/lua/callbacks.h
@@ -67,6 +67,8 @@ namespace nVerliHub {
 	int _GetUserHost(lua_State *L);
 	int _GetUserIP(lua_State *L);
 	int _SetUserIP(lua_State *L);
+	int _SetMyINFOFlag(lua_State *L);
+	int _UnsetMyINFOFlag(lua_State *L);
 	int _IsSecConn(lua_State *L);
 	int _GetTLSVer(lua_State *L);
 	int _IsUserOnline(lua_State *L);

plugins/lua/cluainterpreter.cpp
@@ -100,6 +100,8 @@ bool cLuaInterpreter::Init()
 	RegisterFunction("GetUserHost", &_GetUserHost);
 	RegisterFunction("GetUserIP", &_GetUserIP);
 	RegisterFunction("SetUserIP", &_SetUserIP);
+	RegisterFunction("SetMyINFOFlag", &_SetMyINFOFlag);
+	RegisterFunction("UnsetMyINFOFlag", &_UnsetMyINFOFlag);
 	RegisterFunction("IsSecConn", &_IsSecConn);
 	RegisterFunction("GetTLSVer", &_GetTLSVer);
 	RegisterFunction("IsUserOnline", &_IsUserOnline);
@@ -212,7 +214,7 @@ void cLuaInterpreter::ReportLuaError(const char *error)
 		toall.append(_("Unknown error"));
 
 	string start, end;
-	serv->mP.Create_PMForBroadcast(start, end, serv->mC.opchat_name, serv->mC.opchat_name, toall, false); // dont reserve for pipe, buffer is copied before adding pipe
+	serv->mP.Create_PMForBroadcast(start, end, serv->mC.opchat_name, serv->mC.opchat_name, toall);
 	serv->SendToAllWithNick(start, end, cpiLua::me->err_class, eUC_MASTER); // use err_class here
 	cpiLua::me->ReportLuaError(toall); // also write to err file
 }

plugins/lua/cpilua.cpp
@@ -514,11 +514,11 @@ bool cpiLua::OnParsedMsgSearch(cConnDC *conn, cMessageDC *msg)
 				break;
 
 			case eDC_TTHS:
-				server->mP.Create_Search(data, msg->ChunkString(eCH_SA_ADDR), msg->ChunkString(eCH_SA_TTH), false, false); // dont reserve for pipe, we are not sending this
+				server->mP.Create_Search(data, msg->ChunkString(eCH_SA_ADDR), msg->ChunkString(eCH_SA_TTH), false);
 				break;
 
 			case eDC_TTHS_PAS:
-				server->mP.Create_Search(data, msg->ChunkString(eCH_SP_NICK), msg->ChunkString(eCH_SP_TTH), true, false); // dont reserve for pipe, we are not sending this
+				server->mP.Create_Search(data, msg->ChunkString(eCH_SP_NICK), msg->ChunkString(eCH_SP_TTH), true);
 				break;
 
 			default:

commit 1757850437d60ef9e68e7ea295085c128131f4b9

Oops, broke SearchRule parameters in commit bdb68eb

CMakeLists.txt
@@ -32,7 +32,7 @@ message(STATUS)
 SET(VERLIHUB_VERSION_MAJOR 1) # note: these must be numbers, else compilation will fail
 SET(VERLIHUB_VERSION_MINOR 3)
 SET(VERLIHUB_VERSION_PATCH 1)
-SET(VERLIHUB_VERSION_TWEAK 4)
+SET(VERLIHUB_VERSION_TWEAK 5)
 SET(VERLIHUB_VERSION "${VERLIHUB_VERSION_MAJOR}.${VERLIHUB_VERSION_MINOR}.${VERLIHUB_VERSION_PATCH}.${VERLIHUB_VERSION_TWEAK}")
 #SET(VERLIHUB_SOVERSION "${VERLIHUB_VERSION_MAJOR}.${VERLIHUB_VERSION_MINOR}.${VERLIHUB_VERSION_PATCH}")
 SET(PROJECT_NAME "verlihub")

src/cdcproto.cpp
@@ -1386,71 +1386,71 @@ int cDCProto::DC_MyINFO(cMessageDC *msg, cConnDC *conn)
 		conn->SetLSFlag(eLS_MYINFO);
 
 		if (conn->mFeatures & eSF_SEARRULE) { // send search rule command
-			temp.clear();
-			temp.append("Min ");
-			temp.append(StringFrom(mS->mC.min_search_chars));
-			temp.append("$$Max ");
-			temp.append(StringFrom(mS->mC.max_len_search));
-			temp.append("$$Num ");
-			temp.append(StringFrom(mS->mC.search_number));
-			temp.append("$$Int ");
+			string pars;
+			pars.append("Min ");
+			pars.append(StringFrom(mS->mC.min_search_chars));
+			pars.append("$$Max ");
+			pars.append(StringFrom(mS->mC.max_len_search));
+			pars.append("$$Num ");
+			pars.append(StringFrom(mS->mC.search_number));
+			pars.append("$$Int ");
 
 			if ((conn->mpUser->mClass >= int(eUC_NORMUSER)) && (conn->mpUser->mClass >= mS->mC.min_class_use_hub)) {
 				switch (conn->mpUser->mClass) {
 					case eUC_REGUSER:
-						temp.append(StringFrom(mS->mC.int_search_reg));
+						pars.append(StringFrom(mS->mC.int_search_reg));
 						break;
 
 					case eUC_VIPUSER:
-						temp.append(StringFrom(mS->mC.int_search_vip));
+						pars.append(StringFrom(mS->mC.int_search_vip));
 						break;
 
 					case eUC_OPERATOR:
 					case eUC_CHEEF:
 					case eUC_ADMIN:
-						temp.append(StringFrom(mS->mC.int_search_op));
+						pars.append(StringFrom(mS->mC.int_search_op));
 						break;
 
 					case eUC_MASTER:
-						temp.append(1, '0');
+						pars.append(1, '0');
 						break;
 
 					default:
-						temp.append(StringFrom(mS->mC.int_search));
+						pars.append(StringFrom(mS->mC.int_search));
 						break;
 				}
 			} else {
-				temp.append("-1");
+				pars.append("-1");
 			}
 
-			temp.append("$$IntPas ");
+			pars.append("$$IntPas ");
 
 			if ((conn->mpUser->mClass >= int(eUC_NORMUSER)) && (conn->mpUser->mClass >= mS->mC.min_class_use_hub_passive)) {
 				switch (conn->mpUser->mClass) {
 					case eUC_REGUSER:
-						temp.append(StringFrom(mS->mC.int_search_reg_pas));
+						pars.append(StringFrom(mS->mC.int_search_reg_pas));
 						break;
 
 					case eUC_VIPUSER:
-						temp.append(StringFrom(mS->mC.int_search_vip));
+						pars.append(StringFrom(mS->mC.int_search_vip));
 						break;
 
 					case eUC_OPERATOR:
 					case eUC_CHEEF:
 					case eUC_ADMIN:
-						temp.append(StringFrom(mS->mC.int_search_op));
+						pars.append(StringFrom(mS->mC.int_search_op));
 						break;
 
 					case eUC_MASTER:
-						temp.append(1, '0');
+						pars.append(1, '0');
 						break;
 
 					default:
-						temp.append(StringFrom(mS->mC.int_search_pas));
+						pars.append(StringFrom(mS->mC.int_search_pas));
 						break;
 				}
 			} else {
-				temp.append("-1");
+				pars.append("-1");
 			}
 
 			share_check = 0;
@@ -1473,11 +1473,11 @@ int cDCProto::DC_MyINFO(cMessageDC *msg, cConnDC *conn)
 			}
 
 			if (share_check > 0) { // todo: mS->mC.min_share_factor_passive
-				temp.append("$$Share ");
-				temp.append(StringFrom(share_check * 1024 * 1024));
+				pars.append("$$Share ");
+				pars.append(StringFrom(share_check * 1024 * 1024));
 			}
 
-			Create_SearchRule(temp, temp);
+			Create_SearchRule(temp, pars);
 			conn->Send(temp, true);
 		}
 