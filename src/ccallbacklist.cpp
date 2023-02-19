292b9c172855a9170d963b64a53dee1595e73bbc
Aleksandr Zenkov <webmaster@feardc.net>
2023-02-19 11:42:30 +0100

    Fix #195

--- src/ccallbacklist.cpp
+++ src/ccallbacklist.cpp
@@ -1,6 +1,6 @@
 /*
 	Copyright (C) 2003-2005 Daniel Muller, dan at verliba dot cz
-	Copyright (C) 2006-2022 Verlihub Team, info at verlihub dot net
+	Copyright (C) 2006-2023 Verlihub Team, info at verlihub dot net
 
 	Verlihub is free software; You can redistribute it
 	and modify it under the terms of the GNU General
@@ -25,7 +25,7 @@
 namespace nVerliHub {
 	namespace nPlugin {
 
-cCallBackList::cCallBackList(cPluginManager *mgr, string id):
+cCallBackList::cCallBackList(cPluginManager *mgr, const string &id):
 	mMgr(mgr),
 	mCallOne(mMgr, this),
 	mName(id)
