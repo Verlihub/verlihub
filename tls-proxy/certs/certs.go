efb02edcfad5b6ebe120db1af43c24202460816b
RoLex <webmaster@feardc.net>
2025-08-15 16:52:47 +0200

    Fuck, still need in here

---
+++ tls-proxy/certs/certs.go
@@ -0,0 +1,117 @@
+/*
+	Copyright (C) 2019-2021 Dexo, dexo at verlihub dot net
+	Copyright (C) 2019-2025 Verlihub Team, info at verlihub dot net
+
+	This is free software; You can redistribute it
+	and modify it under the terms of the GNU General
+	Public License as published by the Free Software
+	Foundation, either version 3 of the license, or at
+	your option any later version.
+
+	It is distributed in the hope that it will be
+	useful, but without any warranty, without even the
+	implied warranty of merchantability or fitness for
+	a particular purpose. See the GNU General Public
+	License for more details.
+
+	Please see https://www.gnu.org/licenses/ for a copy
+	of the GNU General Public License.
+*/
+
+package certs
+
+import (
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/x509"
+	"crypto/x509/pkix"
+	"encoding/pem"
+	"errors"
+	"fmt"
+	"math/big"
+	"net"
+	"time"
+)
+
+func Generate(host, org string) (cert, key []byte, _ error) {
+	// generate a new key-pair
+	rootKey, err := rsa.GenerateKey(rand.Reader, 2048)
+
+	if err != nil {
+		return nil, nil, err
+	}
+
+	rootCertTmpl, err := certTemplate(org)
+
+	if err != nil {
+		return nil, nil, err
+	}
+
+	// describe what the certificate will be used for
+	rootCertTmpl.IsCA = true
+	rootCertTmpl.KeyUsage = x509.KeyUsageCertSign | x509.KeyUsageDigitalSignature
+	rootCertTmpl.ExtKeyUsage = []x509.ExtKeyUsage{x509.ExtKeyUsageServerAuth, x509.ExtKeyUsageClientAuth}
+
+	if ip := net.ParseIP(host); ip != nil {
+		rootCertTmpl.IPAddresses = []net.IP{ip}
+	} else {
+		rootCertTmpl.DNSNames = []string{host}
+	}
+
+	_, rootCertPEM, err := createCert(rootCertTmpl, rootCertTmpl, &rootKey.PublicKey, rootKey)
+
+	if err != nil {
+		return nil, nil, fmt.Errorf("Error creating certificate: %v", err)
+	}
+
+	// pem encode the private key
+	rootKeyPEM := pem.EncodeToMemory(&pem.Block{
+		Type: "RSA PRIVATE KEY", Bytes: x509.MarshalPKCS1PrivateKey(rootKey),
+	})
+
+	return rootCertPEM, rootKeyPEM, nil
+}
+
+// helper function to create a cert template with a serial number and other required fields
+func certTemplate(org string) (*x509.Certificate, error) {
+	// generate a random serial number, a real cert authority would have some logic behind this
+	serialNumberLimit := new(big.Int).Lsh(big.NewInt(1), 128)
+	serialNumber, err := rand.Int(rand.Reader, serialNumberLimit)
+
+	if err != nil {
+		return nil, errors.New("Failed to generate serial number: " + err.Error())
+	}
+
+	tmpl := x509.Certificate{
+		SerialNumber: serialNumber,
+		Subject: pkix.Name{Organization: []string{org}},
+		SignatureAlgorithm: x509.SHA256WithRSA,
+		NotBefore: time.Now(),
+		NotAfter: time.Now().Add(time.Hour * 24 * 356),
+		BasicConstraintsValid: true,
+	}
+
+	return &tmpl, nil
+}
+
+func createCert(template, parent *x509.Certificate, pub interface{}, parentPriv interface{}) (cert *x509.Certificate, certPEM []byte, err error) {
+	certDER, err := x509.CreateCertificate(rand.Reader, template, parent, pub, parentPriv)
+
+	if err != nil {
+		return
+	}
+
+	// parse the resulting certificate so we can use it again
+	cert, err = x509.ParseCertificate(certDER)
+
+	if err != nil {
+		return
+	}
+
+	// pem encode the certificate, this is a standard tls encoding
+	b := pem.Block{Type: "CERTIFICATE", Bytes: certDER}
+	certPEM = pem.EncodeToMemory(&b)
+	return
+}
+
+// end of file
---
+++ tls-proxy/dcproxy/dcproxy.go
@@ -0,0 +1,418 @@
+/*
+	Copyright (C) 2019-2021 Dexo, dexo at verlihub dot net
+	Copyright (C) 2019-2025 Verlihub Team, info at verlihub dot net
+
+	This is free software; You can redistribute it
+	and modify it under the terms of the GNU General
+	Public License as published by the Free Software
+	Foundation, either version 3 of the license, or at
+	your option any later version.
+
+	It is distributed in the hope that it will be
+	useful, but without any warranty, without even the
+	implied warranty of merchantability or fitness for
+	a particular purpose. See the GNU General Public
+	License for more details.
+
+	Please see https://www.gnu.org/licenses/ for a copy
+	of the GNU General Public License.
+*/
+
+package dcproxy
+
+import (
+	"bytes"
+	"crypto/tls"
+	"io"
+	"io/ioutil"
+	"log"
+	"net"
+	"net/http"
+	_ "net/http/pprof"
+	"os"
+	"sync"
+	"time"
+
+	"github.com/verlihub/tls-proxy/certs"
+	"github.com/verlihub/tls-proxy/metrics"
+)
+
+/*
+func init() { // no longer needed in 1.13
+	os.Setenv("GODEBUG", os.Getenv("GODEBUG") + ",tls13=1")
+}
+*/
+
+type Config struct {
+	HubAddr string
+	HubNetwork string
+	Hosts []string
+	Cert string
+	Key string
+	CertOrg string
+	CertHost string
+	PProf string
+	Metrics string
+	LogErrors bool
+	Wait time.Duration
+	Buffer int
+	MinVer int
+	NoSendIP bool
+}
+
+type Proxy struct {
+	c Config
+	tls *tls.Config
+
+	wg sync.WaitGroup
+	lis []net.Listener
+}
+
+func New(c Config) (*Proxy, error) {
+	if c.HubAddr == "" {
+		c.HubNetwork = "tcp4"
+		c.HubAddr = "127.0.0.1:411"
+	} else if c.HubNetwork == "" {
+		c.HubNetwork = "tcp4"
+	}
+
+	if c.Buffer == 0 {
+		c.Buffer = 10
+	}
+
+	p := &Proxy{c: c}
+
+	if c.Cert == "" || c.Key == "" {
+		c.Cert = "hub.crt"
+		c.Key = "hub.key"
+	}
+
+	if _, err := os.Stat(c.Key); os.IsNotExist(err) {
+		log.Println("Generating certificates:", c.Cert, c.Key)
+
+		if c.CertOrg == "" {
+			c.CertOrg = "DCProxy"
+		}
+
+		if c.CertHost == "" {
+			c.CertHost = "localhost"
+		}
+
+		cert, key, err := certs.Generate(c.CertHost, c.CertOrg)
+
+		if err != nil {
+			return nil, err
+		}
+
+		if err = ioutil.WriteFile(c.Cert, cert, 0644); err != nil {
+			return nil, err
+		}
+
+		if err = ioutil.WriteFile(c.Key, key, 0600); err != nil {
+			return nil, err
+		}
+	}
+
+	log.Println("Using certificates:", c.Cert, c.Key)
+	var err error
+	var minver uint16
+
+	switch c.MinVer {
+		case 0:
+			minver = tls.VersionTLS10
+		case 1:
+			minver = tls.VersionTLS11
+		case 2:
+			minver = tls.VersionTLS12
+		default:
+			minver = tls.VersionTLS13
+	}
+
+	tlsConfig := &tls.Config{NextProtos: []string{"nmdc"}, MinVersion: minver}
+	tlsConfig.Certificates = make([]tls.Certificate, 1)
+	tlsConfig.Certificates[0], err = tls.LoadX509KeyPair(c.Cert, c.Key)
+
+	if err != nil {
+		return nil, err
+	}
+
+	p.tls = tlsConfig
+	return p, nil
+}
+
+func (p *Proxy) Wait() {
+	p.wg.Wait()
+}
+
+func (p *Proxy) Close() error {
+	for _, l := range p.lis {
+		_ = l.Close()
+	}
+
+	p.lis = nil
+	return nil
+}
+
+func (p *Proxy) Run() error {
+	if p.c.PProf != "" {
+		log.Println("Enabling profiler on", p.c.PProf)
+
+		go func() {
+			if err := http.ListenAndServe(p.c.PProf, nil); err != nil {
+				log.Println("Failed to enable profiler:", err)
+			}
+		}()
+	}
+
+	if p.c.Metrics != "" {
+		log.Println("Serving metrics on", p.c.Metrics)
+
+		go func() {
+			if err := metrics.ListenAndServe(p.c.Metrics); err != nil {
+				log.Println("Failed to serve metrics:", err)
+			}
+		}()
+	}
+
+	for _, host := range p.c.Hosts {
+		l, err := net.Listen("tcp4", host)
+
+		if err != nil {
+			_ = p.Close()
+			return err
+		}
+
+		p.lis = append(p.lis, l)
+	}
+
+	for i, l := range p.lis {
+		p.wg.Add(1)
+		l := l
+		log.Println("Proxying", p.c.Hosts[i], "to", p.c.HubAddr)
+
+		go func() {
+			defer p.wg.Done()
+			p.acceptOn(l)
+		}()
+	}
+
+	return nil
+}
+
+func (p *Proxy) acceptOn(l net.Listener) {
+	for {
+		c, err := l.Accept()
+
+		if err != nil {
+			if p.c.LogErrors {
+				log.Println(err)
+			}
+
+			metrics.ConnError.Add(1)
+			continue
+		}
+
+		metrics.ConnAccepted.Add(1)
+
+		go func() {
+			err := p.serve(c)
+
+			if err != nil && err != io.EOF {
+				metrics.ConnError.Add(1)
+
+				if p.c.LogErrors {
+					log.Println(c.RemoteAddr(), err)
+				}
+			}
+		}()
+	}
+}
+
+type timeoutErr interface {
+	Timeout() bool
+}
+
+func (p *Proxy) serve(c net.Conn) error {
+	metrics.ConnOpen.Add(1)
+
+	defer func() {
+		_ = c.Close()
+		metrics.ConnOpen.Add(-1)
+	}()
+
+	buf := make([]byte, 1024)
+	i := copy(buf, "$MyIP ")
+	i += copy(buf[i:], c.RemoteAddr().(*net.TCPAddr).IP.String())
+	i += copy(buf[i:], " 0.0|")
+
+	if p.tls == nil || p.c.Wait <= 0 { // no auto detection
+		metrics.ConnInsecure.Add(1)
+		metrics.ConnOpenInsecure.Add(1)
+		defer metrics.ConnOpenInsecure.Add(-1)
+		return p.writeAndStream(buf[:i], c, i)
+	}
+
+	err := c.SetReadDeadline(time.Now().Add(p.c.Wait))
+
+	if err != nil {
+		return err
+	}
+
+	start := time.Now()
+	n, err := c.Read(buf[i:])
+	_ = c.SetReadDeadline(time.Time{})
+
+	if e, ok := err.(timeoutErr); ok && e.Timeout() { // has to be plain nmdc
+		metrics.ConnInsecure.Add(1)
+		metrics.ConnOpenInsecure.Add(1)
+		defer metrics.ConnOpenInsecure.Add(-1)
+		return p.writeAndStream(buf[:i], c, i)
+	}
+
+	if err != nil {
+		return err
+	}
+
+	buf = buf[:i+n]
+
+	if n >= 2 && buf[i] == 0x16 && buf[i+1] == 0x03 {
+		tlsBuf := buf[i:]
+		c = &multiReadConn{r: io.MultiReader(bytes.NewReader(tlsBuf), c), Conn: c}
+		tc := tls.Server(c, p.tls) // tls handshake
+		defer tc.Close()
+		err = tc.Handshake()
+
+		if err != nil {
+			return err
+		}
+
+		buf[i-4] = '1' // set version
+		state := tc.ConnectionState()
+
+		switch state.Version {
+			case tls.VersionTLS13:
+				buf[i-2] = '3'
+			case tls.VersionTLS12:
+				buf[i-2] = '2'
+			case tls.VersionTLS11:
+				buf[i-2] = '1'
+		}
+
+		buf = buf[:i]
+		dt := time.Since(start).Seconds()
+		metrics.ConnTLS.Add(1)
+		metrics.ConnOpenTLS.Add(1)
+		defer metrics.ConnOpenTLS.Add(-1)
+		metrics.ConnTLSHandshake.Observe(dt)
+		return p.writeAndStream(buf, tc, i)
+	}
+
+	metrics.ConnInsecure.Add(1)
+	metrics.ConnOpenInsecure.Add(1)
+	defer metrics.ConnOpenInsecure.Add(-1)
+	return p.writeAndStream(buf, c, i)
+}
+
+type multiReadConn struct {
+	r io.Reader
+	net.Conn
+}
+
+func (c *multiReadConn) Read(b []byte) (int, error) {
+	return c.r.Read(b)
+}
+
+func (p *Proxy) dialHub() (net.Conn, error) {
+	return net.Dial(p.c.HubNetwork, p.c.HubAddr)
+}
+
+func (p *Proxy) writeAndStream(b []byte, c io.ReadWriteCloser, i int) error {
+	h, err := p.dialHub()
+
+	if err != nil {
+		return err
+	}
+
+	defer h.Close()
+
+	if p.c.NoSendIP {
+		b = b[i:]
+	}
+
+	if len(b) > 0 {
+		_, err = h.Write(b)
+
+		if err != nil {
+			return err
+		}
+	}
+
+	return p.stream(c, h)
+}
+
+func (p *Proxy) stream(c, h io.ReadWriteCloser) error {
+	closeBoth := func() {
+		_ = c.Close()
+		_ = h.Close()
+	}
+
+	defer closeBoth()
+
+	go func() {
+		defer closeBoth()
+		_, _ = p.copyBuffer(h, c, metrics.ConnRx)
+	}()
+
+	_, _ = p.copyBuffer(c, h, metrics.ConnTx)
+	return nil
+}
+
+// this was copied from io package and modified to add instrumentation
+func (p *Proxy) copyBuffer(dst io.Writer, src io.Reader, cnt metrics.Counter) (written int64, err error) {
+	size := p.c.Buffer * 1024
+
+	if l, ok := src.(*io.LimitedReader); ok && int64(size) > l.N {
+		if l.N < 1 {
+			size = 1
+		} else {
+			size = int(l.N)
+		}
+	}
+
+	buf := make([]byte, size)
+
+	for {
+		nr, er := src.Read(buf)
+
+		if nr > 0 {
+			nw, ew := dst.Write(buf[0:nr])
+			cnt.Add(float64(nw))
+
+			if nw > 0 {
+				written += int64(nw)
+			}
+
+			if ew != nil {
+				err = ew
+				break
+			}
+
+			if nr != nw {
+				err = io.ErrShortWrite
+				break
+			}
+		}
+
+		if er != nil {
+			if er != io.EOF {
+				err = er
+			}
+
+			break
+		}
+	}
+
+	return written, err
+}
+
+// end of file
