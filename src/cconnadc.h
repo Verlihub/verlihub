f08aabb18fd81c20b75f8f987cdff4d0167b42a9
af1987 <97942066+af1987@users.noreply.github.com>
2026-08-12 08:36:09 +0200

    Add ADC-native connection container

---
+++ src/cconnadc.h
@@ -0,0 +1,44 @@
+/*
+	Copyright (C) 2006-2026 Verlihub Team, info at verlihub dot net
+
+	Verlihub is free software; You can redistribute it
+	and modify it under the terms of the GNU General
+	Public License as published by the Free Software
+	Foundation, either version 3 of the license, or at
+	your option any later version.
+*/
+
+#ifndef CCONNADC_H
+#define CCONNADC_H
+
+#include "casyncconn.h"
+
+namespace nVerliHub {
+	namespace nTables {
+		class cRegUserInfo;
+	};
+
+	namespace nSocket {
+
+/**
+ * Minimal client-hub connection container for ADC.
+ *
+ * Protocol login state, negotiated features, SID/CID/PID and INF data live in
+ * cADCSessionManager. This object deliberately contains none of cConnDC's
+ * NMDC login flags, $Supports bits, pipe framing, DC protocol flood enums or
+ * $ForceMove redirect behaviour.
+ */
+class cConnADC : public cAsyncConn
+{
+	public:
+		cConnADC(int sd = 0, cAsyncSocketServer *server = NULL);
+		virtual ~cConnADC();
+
+		/** Registration record loaded for the ADC nickname, if any. */
+		nTables::cRegUserInfo *mRegInfo;
+};
+
+	}; // namespace nSocket
+}; // namespace nVerliHub
+
+#endif
