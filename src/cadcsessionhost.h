eea0c4710b54e24fa039a44c41e37ac451319d12
af1987 <97942066+af1987@users.noreply.github.com>
2026-08-12 09:40:17 +0200

    Add native ADC session lifecycle host

---
+++ src/cadcsessionhost.h
@@ -0,0 +1,36 @@
+/*
+	Copyright (C) 2006-2026 Verlihub Team, info at verlihub dot net
+
+	Verlihub is free software; You can redistribute it
+	and modify it under the terms of the GNU General
+	Public License as published by the Free Software
+	Foundation, either version 3 of the license, or at
+	your option any later version.
+*/
+
+#ifndef CADCSESSIONHOST_H
+#define CADCSESSIONHOST_H
+
+namespace nVerliHub {
+	namespace nProtocol {
+		struct sADCSession;
+	};
+
+	namespace nSocket {
+		class cAsyncConn;
+
+/** Native ADC session lifecycle sink, independent from cUser/cConnDC. */
+class cADCSessionHost
+{
+	public:
+		virtual ~cADCSessionHost() {}
+		virtual void OnADCSessionNormal(cAsyncConn *conn,
+			const nProtocol::sADCSession &session) = 0;
+		virtual void OnADCSessionDetach(cAsyncConn *conn,
+			const nProtocol::sADCSession &session) = 0;
+};
+
+	}; // namespace nSocket
+}; // namespace nVerliHub
+
+#endif
