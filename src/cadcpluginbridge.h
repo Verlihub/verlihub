31de457129b1223f978c6970c68c1d0da83031b9
af1987 <97942066+af1987@users.noreply.github.com>
2026-08-12 08:57:14 +0200

    Add ADC plugin dispatch bridge

---
+++ src/cadcpluginbridge.h
@@ -0,0 +1,22 @@
+#ifndef CADCPLUGINBRIDGE_H
+#define CADCPLUGINBRIDGE_H
+
+namespace nVerliHub {
+namespace nSocket { class cConnADC; }
+namespace nProtocol { class cMessageADC; }
+namespace nPlugin {
+class cPluginManager;
+
+class cADCPluginBridge
+{
+public:
+	static bool OnConnect(cPluginManager *manager, nSocket::cConnADC *conn);
+	static bool OnMessage(cPluginManager *manager, nSocket::cConnADC *conn,
+		nProtocol::cMessageADC *msg);
+	static void OnDisconnect(cPluginManager *manager, nSocket::cConnADC *conn);
+};
+
+} // namespace nPlugin
+} // namespace nVerliHub
+
+#endif
