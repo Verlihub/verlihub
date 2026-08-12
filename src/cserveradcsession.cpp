b089bda7c45b7695cb03a3f715a8aee18c5afe21
af1987 <97942066+af1987@users.noreply.github.com>
2026-08-12 09:43:29 +0200

    Fix ADC session bridge license header

--- src/cserveradcsession.cpp
+++ src/cserveradcsession.cpp
@@ -3,7 +3,9 @@
 
 	Verlihub is free software; You can redistribute it
 	and modify it under the terms of the GNU General
-	Public License for more details.
+	Public License as published by the Free Software
+	Foundation, either version 3 of the license, or at
+	your option any later version.
 */
 
 #include "cserveradc.h"
